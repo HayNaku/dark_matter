00882cf9aac4e1b013fa7cde9e6e97faa20b1572
HayNaku <markanthony_tungal@git.com>
2022-11-21 12:07:11 +0800

    New Folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './layout/login/login.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { TopbarComponent } from './layout/topbar/topbar.component';
+import { PagesComponent } from './pages/pages.component';
+import { CustomerComponent } from './pages/customer/customer.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { DetailsComponent } from './pages/customer/details/details.component';
+import { FormsComponent } from './pages/customer/forms/forms.component';
+import { TableComponent } from './pages/customer/table/table.component';
+import { TablesComponent } from './pages/products/tables/tables.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    LoginComponent,
+    SidebarComponent,
+    TopbarComponent,
+    PagesComponent,
+    CustomerComponent,
+    ProductsComponent,
+    DetailsComponent,
+    FormsComponent,
+    TableComponent,
+    TablesComponent
   ],
   imports: [
     BrowserModule,
